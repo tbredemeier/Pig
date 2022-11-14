4c6979c7b40b9b6d4869f46ce9f4dbe9bcb0edb6
Tom Bredemeier <tbredemeier@barrington220.org>
2022-11-14 09:38:17 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Tom Bredemeier on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
