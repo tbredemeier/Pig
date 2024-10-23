89583d096e64fa13e8ed18d72ecf5438bdc45526
Tom Bredemeier <tbredemeier@barrington220.org>
2024-10-23 10:51:50 -0500

    Added dice image display

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -10,12 +10,20 @@ import SwiftUI
 struct ContentView: View {
     @State private var turnScore = 0
     @State private var gameScore = 0
+    @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
         ZStack {
             Color.gray.opacity(0.7).ignoresSafeArea()
             VStack {
                 Image("Pig").resizable().frame(width: 150, height: 150)
                 CustomText(text: "Pig")
+                Image("pips \(randomValue)")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .rotationEffect(.degrees(rotation))
+                    .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                    .padding(50)
                 CustomText(text: "Turn Score: \(turnScore)")
                 CustomText(text: "Game Score: \(gameScore)")
                 Spacer()
