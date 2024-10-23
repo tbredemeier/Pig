dc19a64ddad1ec26362d36225e2ded9199b05a98
Tom Bredemeier <tbredemeier@barrington220.org>
2024-10-23 11:54:19 -0500

    Added basic game mechanics

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -27,16 +27,47 @@ struct ContentView: View {
                 CustomText(text: "Turn Score: \(turnScore)")
                 HStack {
                     Button("Roll") {
-                        
+                        chooseRandom(times: 3)
+                        withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                            rotation += 360
+                        }
                     }
                     .buttonStyle(CustomButtonStyle())
                     Button("Hold") {
-                        
+                        gameScore += turnScore
+                        endTurn()
+                        withAnimation(.easeInOut(duration: 1)) {
+                            rotation += 360
+                        }
                     }
                     .buttonStyle(CustomButtonStyle())
                 }
                 CustomText(text: "Game Score: \(gameScore)")
                 Spacer()
+            }
+        }
+    }
+    
+    func endTurn() {
+        turnScore = 0
+        randomValue = 0
+    }
+    
+    func chooseRandom(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
+        }
+        if times == 0 {
+            if randomValue == 1 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    endTurn()
+                }
+            }
+            else {
+                turnScore += randomValue
             }
         }
     }
