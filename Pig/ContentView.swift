430b7d93aa67214df6d7c984c2d0a10cdd7ec871
Tom Bredemeier <tbredemeier@barrington220.org>
2024-10-23 11:37:46 -0500

    Added custom button style

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -25,6 +25,16 @@ struct ContentView: View {
                     .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
                     .padding(50)
                 CustomText(text: "Turn Score: \(turnScore)")
+                HStack {
+                    Button("Roll") {
+                        
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                    Button("Hold") {
+                        
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                }
                 CustomText(text: "Game Score: \(gameScore)")
                 Spacer()
             }
@@ -36,6 +46,18 @@ struct CustomText: View {
     let text: String
     var body: some View {
         Text(text).font(Font.custom("Marker Felt", size: 36))
+    }
+}
+
+struct CustomButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 50)
+            .font(Font.custom("Marker Felt", size: 24))
+            .padding()
+            .background(.red).opacity(configuration.isPressed ? 0.0 : 1.0)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
