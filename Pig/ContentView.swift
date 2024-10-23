3cf369b05e97faed238d19c59ecd358746b61e1d
Tom Bredemeier <tbredemeier@barrington220.org>
2024-10-23 10:39:04 -0500

    Added custom text view

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -8,15 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var turnScore = 0
+    @State private var gameScore = 0
     var body: some View {
         ZStack {
             Color.gray.opacity(0.7).ignoresSafeArea()
             VStack {
                 Image("Pig").resizable().frame(width: 150, height: 150)
-                Text("Pig")
+                CustomText(text: "Pig")
+                CustomText(text: "Turn Score: \(turnScore)")
+                CustomText(text: "Game Score: \(gameScore)")
                 Spacer()
             }
         }
+    }
+}
+
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("Marker Felt", size: 36))
     }
 }
 
